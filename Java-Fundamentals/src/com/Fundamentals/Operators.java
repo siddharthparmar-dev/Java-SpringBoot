89256061c92636b0ddc506167a3ac8647bdbce86
siddharth parmar <Siddharthparmar.dev@gmail.com>
2026-02-15 15:09:17 +0530

    added operator.java

--- Java-Fundamentals/src/com/Fundamentals/Operators.java
+++ Java-Fundamentals/src/com/Fundamentals/Operators.java
@@ -6,9 +6,25 @@ public class Operators {
         int a = 5;
         int b = 10;
         int c = 15;
+
+
         //arithematic operator
-        System.out.println();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b%c);
+        System.out.println(c/a);
 
+        //relational operator (boolean result: true or false)
+        // == . < , > , <= , >= , !=
+        System.out.println(5 == 6);  // false
+        System.out.println(5 < 6);   // true
+        System.out.println(5 > 6);   // false
+        System.out.println(5 <= 6);  // true
+        System.out.println(5 >= 6);  // false
+        System.out.println(5 != 6);  // true
 
+        //logical operator
+        // || , && , !
     }
 }
